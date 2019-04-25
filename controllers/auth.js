12c42bcf15719bd3a332026f4e616faf15f110a9
Hassan <hassanharga@gmail.com>
2019-04-25 12:05:48 +0200

    b

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,7 +34,7 @@ module.exports = {
     let userName = await User.findOne({ username: req.body.username }); //checking if the username is exists
     if (userName) {
       return res.status(httpStatus.CONFLICT).json({ message: 'Username is already exists' });
-      
+
     }
     return bcrypt.hash(value.password, 10, (err, hash) => {   //encrypting the password
       if (err) {
@@ -47,7 +47,7 @@ module.exports = {
       }
       User.create(body).then(user => {
         let token = jwt.sign({ data: user }, config.secret, { //create token
-          expiresIn: 120
+          expiresIn: "1h"
         });
         res.cookie("token", token);
         res.status(httpStatus.CREATED).json({ message: "user created successfully", user, token });
@@ -55,5 +55,39 @@ module.exports = {
         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "error creating user" });
       })
     })
+  },
+  async loginUser(req, res) {
+    console.log(req.body);
+    if (!req.body.user || !req.body.pass) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'no empty fields allowed' });
+    }
+    await User.findOne({ username: req.body.user })
+      .then(user => {
+        if (!user) {
+          res.status(httpStatus.NOT_FOUND).json({ message: 'username not found' });
+        }
+        else {
+          bcrypt.compare(req.body.pass, user.password)
+            .then(result => {
+              if (!result) {
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'password is incorrect' });
+              }
+              else {
+                let token = jwt.sign({ data: user }, config.secret, {
+                  expiresIn: "1h"
+                })
+                res.cookie("token", token);
+                res.status(httpStatus.OK).json({ message: 'Login sucessful', user, token });
+              }
+            })
+            .catch(error => {
+              res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'error finding user' });
+            })
+        }
+
+      })
+      .catch(err => {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'error occured' });
+      })
   }
 };
