54e9ca68fce65d27779ea8f2538e51a6d1f98c10
Hassan <hassanharga@gmail.com>
2019-05-23 15:10:13 +0200

    Add get all message router

--- controllers/message.js
+++ controllers/message.js
@@ -6,6 +6,28 @@ let httpStatus = require('http-status-codes');
 
 
 module.exports = {
+    async getAllMessages(req, res) {
+        const { sender_Id, receiver_Id } = req.params;
+        // console.log(sender_Id, receiver_Id);
+        const conversation = await Conversation.findOne({
+            $or: [
+                {
+                    $and: [{'participants.senderId': sender_Id},{'participants.receiverId': receiver_Id}]
+                },
+                {
+                    $and: [{'participants.senderId': receiver_Id},{'participants.receiverId': sender_Id}]
+                },
+            ]
+        }).select('_id')
+
+        // console.log(conversation);
+        if (conversation) {
+            const messages = await Message.findOne({conversationId: conversation._id});
+            res.status(httpStatus.OK).json({message : 'all messages', messages});
+        } else {
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message : 'couldnot get all messages'});
+        }
+    },
     sengMessage(req, res) {
         // console.log(req.user._id);
         const { sender_Id, receiver_Id } = req.params;
@@ -18,6 +40,27 @@ module.exports = {
         },
             async (err, result) => {
                 if (result.length > 0) {
+                    // console.log(result);
+                    await Message.update({
+                        conversationId: result[0]._id
+                    },
+                    {
+                        $push: {
+                            message: {
+                                senderId: req.user._id,
+                                receiverId: req.params.receiver_Id,
+                                sernderName: req.user.username,
+                                receiverName: req.body.receiverName,
+                                body: req.body.message,
+                            }
+                        }
+                    }
+                    ).then(() => {
+                        res.status(httpStatus.OK).json({ message: 'message sent sucessfully', })
+                    })
+                    .catch(() => {
+                        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: 'message not sent', })
+                    })
 
                 } else {
                     const newConversation = new Conversation();
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let messageCtrl = require("../controllers/message");
 let AuthHelper = require('../Helpers/authHelper');
 
+router.get('/chat-message/:sender_Id/:receiver_Id', AuthHelper.authenticate, messageCtrl.getAllMessages);
 router.post('/chat-message/:sender_Id/:receiver_Id', AuthHelper.authenticate, messageCtrl.sengMessage);
 
 module.exports = router;
