427578cdbc5b30dd4082e8e5665d7754ce04d992
HaSsan <hassanharga@gmail.com>
2019-07-01 16:03:20 +0200

    view profile notifications

--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,61 @@
-let User= require('../models/userModel');
+let User = require('../models/userModel');
+let moment = require('moment');
+let httpStatus = require('http-status-codes');
 
-let httpStatus= require('http-status-codes');
 
-
-module.exports= {
-    async getAllUsers(req,res) {
+module.exports = {
+    async getAllUsers(req, res) {
         await User.find({}).populate('posts.postId').populate('following.userFollowed').populate('followers.follower').populate('chatList.receiverId').populate('chatList.msgId').populate('notifications.senderId')
-        .then(users =>{
-            res.status(httpStatus.OK).json({message: "all users" ,users});
-        }).catch(err =>{
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message: "Error While getting Users" ,err});
-        });
+            .then(users => {
+                res.status(httpStatus.OK).json({ message: "all users", users });
+            }).catch(err => {
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "Error While getting Users", err });
+            });
     },
 
-    async getByUserId(req,res) {
+    async getByUserId(req, res) {
         // let id = req.params.id;
-        await User.findOne({_id: req.params.id}).populate('posts.postId').populate('following.userFollowed').populate('followers.follower').populate('chatList.receiverId').populate('chatList.msgId').populate('notifications.senderId')
-        .then(user =>{
-            res.status(httpStatus.OK).json({message: "user by userId" ,user});
-        }).catch(err =>{
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message: "Error While getting Users" ,err});
-        });
+        await User.findOne({ _id: req.params.id }).populate('posts.postId').populate('following.userFollowed').populate('followers.follower').populate('chatList.receiverId').populate('chatList.msgId').populate('notifications.senderId')
+            .then(user => {
+                res.status(httpStatus.OK).json({ message: "user by userId", user });
+            }).catch(err => {
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "Error While getting Users", err });
+            });
     },
 
-    async getByUsername(req,res) {
+    async getByUsername(req, res) {
         console.log(req.params.username);
         // let username = req.params.username;
-        await User.findOne({username: req.params.username}).populate('posts.postId').populate('following.userFollowed').populate('followers.follower').populate('chatList.receiverId').populate('chatList.msgId').populate('notifications.senderId')
-        .then(user =>{
-            res.status(httpStatus.OK).json({message: "user by username" ,user});
-        }).catch(err =>{
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message: "Error While getting Users" ,err});
-        });
+        await User.findOne({ username: req.params.username }).populate('posts.postId').populate('following.userFollowed').populate('followers.follower').populate('chatList.receiverId').populate('chatList.msgId').populate('notifications.senderId')
+            .then(user => {
+                res.status(httpStatus.OK).json({ message: "user by username", user });
+            }).catch(err => {
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "Error While getting Users", err });
+            });
+    },
+
+    async profileView(req, res) {
+        const dateValue = moment().format('YYYY-MM-DD');
+        await User.update({
+            _id: req.body.id,
+            'notifications.date': { $ne: dateValue },
+            'notifications.senderId': { $ne: req.user._id }
+        }, {
+                $push: {
+                    notifications: {
+                        senderId: req.user._id,
+                        message: `${req.user.username} viewed your profile`,
+                        created: new Date(),
+                        date: dateValue,
+                        viewProfile: true
+                    }
+                }
+            }).then(user => {
+                res.status(httpStatus.OK).json({ message: "notification sent" });
+            }).catch(err => {
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "notification error"});
+            });
+        // console.log(req.body);
     }
 
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,6 +6,7 @@ let AuthHelper = require('../Helpers/authHelper');
 router.get('/users', AuthHelper.authenticate, userCtrl.getAllUsers);
 router.get('/user/:id', AuthHelper.authenticate, userCtrl.getByUserId);
 router.get('/username/:username', AuthHelper.authenticate, userCtrl.getByUsername);
+router.post('/user/view-profile', AuthHelper.authenticate, userCtrl.profileView);
 
 
 module.exports = router;
